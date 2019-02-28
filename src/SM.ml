eb2636d8556a10e1fe01a03770662bf39f3c8e04
maryilyina96 <maryilyina96@gmail.com>
2019-02-28 19:37:07 +0700

    implemented stack machine

--- src/SM.ml
+++ src/SM.ml
@@ -22,8 +22,26 @@ type config = int list * Syntax.Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *) 
+let execute_instr (stack, (s, i, o)) instr = match instr with 
+  | BINOP op -> 
+      let y :: x :: rest = stack in 
+      ((Syntax.Expr.eval_binop op x y) :: rest, (s, i, o)) 
+  | CONST c -> (c :: stack, (s, i, o))
+  | READ -> 
+      let z :: rest = i in
+      (z :: stack, (s, rest, o))
+  | WRITE -> 
+      let z :: rest = stack in
+      (rest, (s, i, o @ [z]))
+  | LD x -> ((s x) :: stack, (s, i, o))
+  | ST x -> 
+      let z :: rest = stack in
+      (rest, (Syntax.Expr.update x z s, i, o))
+
+let rec eval conf prg = match prg with
+  | instr :: rest -> eval (execute_instr conf instr) rest
+  | []            -> conf
 
 (* Top-level evaluation
 
@@ -41,4 +59,15 @@ let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
    stack machine
  *)
 
-let compile _ = failwith "Not yet implemented"
+let rec compile stmt =
+    match stmt with
+    | Syntax.Stmt.Read    v       -> [READ; ST v]
+    | Syntax.Stmt.Write   e       -> (compileExpr e) @ [WRITE]
+    | Syntax.Stmt.Assign (v, e)   -> (compileExpr e) @ [ST v]
+    | Syntax.Stmt.Seq    (e1, e2) -> (compile e1) @ (compile e2)
+
+    and compileExpr e =
+        match e with
+        | Syntax.Expr.Const  c         -> [CONST c]
+        | Syntax.Expr.Var    v         -> [LD v]
+        | Syntax.Expr.Binop (op, l, r) -> (compileExpr l) @ (compileExpr r) @ [BINOP op]
