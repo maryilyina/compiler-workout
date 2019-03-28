2d1679a89512fdd11cd1bd9dd4472bfa47975bcc
maryilyina96 <maryilyina96@gmail.com>
2019-03-29 00:54:20 +0700

    implemented if, while, for, skip and repeat

--- src/Language.ml
+++ src/Language.ml
@@ -5,6 +5,7 @@ open GT
 
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
+open List
        
 (* Simple expressions: syntax and semantics *)
 module Expr =
@@ -43,8 +44,36 @@ module Expr =
  
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
-    *)                                                       
-    let eval st expr = failwith "Not yet implemented"
+    *)
+
+    let to_integer bin_val = 
+      if bin_val then 1 else 0
+
+    let to_bool int_val =
+      if int_val != 0 then true else false
+
+    let eval_binop op x y =
+      match op with
+        | "+" -> x + y
+        | "-" -> x - y
+        | "*" -> x * y
+        | "/" -> x / y
+        | "%" -> x mod y
+        | ">"  -> to_integer(x > y)
+        | "<"  -> to_integer(x < y)
+        | ">=" -> to_integer(x >= y)
+        | "<=" -> to_integer(x <= y)
+        | "==" -> to_integer(x = y)
+        | "!=" -> to_integer(x <> y)
+        | "&&" -> to_integer((to_bool x) && (to_bool y))
+        | "!!" -> to_integer((to_bool x) || (to_bool y))
+        | _    -> failwith "Not implemented"
+
+    let rec eval st expr =
+      match expr with 
+        | Const c  -> c
+        | Var v    -> st v
+        | Binop(op, x, y) -> eval_binop op (eval st x) (eval st y)
 
     (* Expression parser. You can use the following terminals:
 
@@ -52,10 +81,32 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string
                                                                                                                   
     *)
-    ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+    let ($) f x = f x
+
+    let binop_parser xs = 
+      List.map (
+        fun x -> ostap (- $(x)), 
+        (fun l r -> Binop (x, l, r))
+      ) xs;;
+
+    ostap (
+      expr: 
+        !(Ostap.Util.expr
+          (fun x -> x)
+          [|
+            `Lefta, binop_parser ["!!"];
+            `Lefta, binop_parser ["&&"];
+            `Nona,  binop_parser ["<="; ">="; "<"; ">"; "=="; "!="];
+            `Lefta, binop_parser ["+"; "-"];
+            `Lefta, binop_parser ["*"; "/"; "%"];
+          |]
+          primary
+        );
+      primary: 
+          x:IDENT   { Var x } 
+        | n:DECIMAL { Const n } 
+        | -"(" expr -")"
     )
-    
   end
                     
 (* Simple statements: syntax and sematics *)
@@ -64,14 +115,14 @@ module Stmt =
 
     (* The type for statements *)
     @type t =
-    (* read into the variable           *) | Read   of string
-    (* write the value of an expression *) | Write  of Expr.t
-    (* assignment                       *) | Assign of string * Expr.t
-    (* composition                      *) | Seq    of t * t 
+    (* read into the variable           *) | Read         of string
+    (* write the value of an expression *) | Write        of Expr.t
+    (* assignment                       *) | Assign       of string * Expr.t
+    (* composition                      *) | Seq          of t * t 
     (* empty statement                  *) | Skip
-    (* conditional                      *) | If     of Expr.t * t * t
-    (* loop with a pre-condition        *) | While  of Expr.t * t
-    (* loop with a post-condition       *) (* add yourself *)  with show
+    (* conditional                      *) | If           of Expr.t * t * t
+    (* loop with a pre-condition        *) | While        of Expr.t * t
+    (* loop with a post-condition       *) | RepeatUntil  of t * Expr.t  with show
                                                                     
     (* The type of configuration: a state, an input stream, an output stream *)
     type config = Expr.state * int list * int list 
@@ -82,13 +133,54 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let rec eval conf stmt = failwith "Not yet implemented"
-                               
+    let rec eval (s, i, o) statement = 
+      match statement with
+        | Read x -> 
+            (let head::tail = i in
+            Expr.update x head s, tail, o)
+        | Write e       -> (s, i, o@[Expr.eval s e])
+        | Assign (x, e) -> (Expr.update x (Expr.eval s e) s, i, o) 
+        | Seq    (a, b) -> eval (eval (s, i, o) a) b  
+        | If (what, first, second)  -> 
+              eval (s, i, o) 
+              (if Expr.eval s what == 0 then second else first)
+        | While (what, body)  ->
+            (if Expr.eval s what == 0 then (s, i, o)
+            else 
+              let repeatition = While (what, body) in 
+              eval (eval (s, i, o) body) repeatition)
+        | RepeatUntil (body, what) ->
+            (let repeatition = While (Expr.Binop ("==", what, Expr.Const 0), body) in
+              eval (eval (s, i, o) body) repeatition)
+
+
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not yet implemented"}
-    )
-      
+      statement: 
+          "read"    "("   x:IDENT        ")"  { Read   x    }
+        | "write"   "("   e:!(Expr.expr) ")"  { Write  e    }
+        | x:IDENT   ":="  e:!(Expr.expr)      { Assign(x, e)}
+        
+        | "if" what:!(Expr.expr) "then" first:!(parse)
+          elif_branch: (%"elif" !(Expr.expr) %"then" !(parse))*
+          els_branch:  (%"else" !(parse))?
+          "fi" { 
+              let els_body = match els_branch with
+                | Some x -> x
+                | _ -> Skip
+              in 
+              let expanded = List.fold_right 
+                  (fun (cond, body) else_body -> 
+                    If (cond, body, else_body)) elif_branch els_body 
+                    in If (what, first, expanded) 
+              }
+        | "while" what:!(Expr.expr) "do" body:!(parse) "od" { While (what, body) }
+        | "repeat" body:!(parse) "until" what:!(Expr.expr) { RepeatUntil (body, what) }
+        | "for" what:!(parse) "," cond:!(Expr.expr) "," step:!(parse) "do" body:!(parse)
+          "od" { Seq (what, While (cond, Seq (body, step))) }
+        | "skip" { Skip };
+      parse: line:statement ";" tail:parse { Seq (line, tail) } | statement
+    ) 
   end
 
 (* The top-level definitions *)
--- src/SM.ml
+++ src/SM.ml
@@ -19,16 +19,46 @@ type prg = insn list
 (* The type for the stack machine configuration: a stack and a configuration from statement
    interpreter
  *)
-type config = int list * Stmt.config
+type config = int list * Language.Stmt.config
 
 (* Stack machine interpreter
 
-     val eval : env -> config -> prg -> config
+     val eval : config -> prg -> config
 
-   Takes an environment, a configuration and a program, and returns a configuration as a result. The
-   environment is used to locate a label to jump to (via method env#labeled <label_name>)
-*)                         
-let rec eval env conf prog = failwith "Not yet implemented"
+   Takes a configuration and a program, and returns a configuration as a result
+ *) 
+ 
+let execute_instr (stack, (s, i, o)) instr = match instr with 
+  | BINOP op -> 
+      let y :: x :: rest = stack in 
+      ((Language.Expr.eval_binop op x y) :: rest, (s, i, o)) 
+  | CONST c -> (c :: stack, (s, i, o))
+  | READ -> 
+      let z :: rest = i in
+      (z :: stack, (s, rest, o))
+  | WRITE -> 
+      let z :: rest = stack in
+      (rest, (s, i, o @ [z]))
+  | LD x -> ((s x) :: stack, (s, i, o))
+  | ST x -> 
+      let z :: rest = stack in
+      (rest, (Language.Expr.update x z s, i, o))
+  | LABEL _ -> (stack, (s, i, o))
+
+let rec eval env conf prg = match prg with
+  | [] -> conf
+  | _  -> (
+    let instr :: rest = prg in 
+    match instr with 
+      (*| LABEL x -> eval env conf rest*)
+      | JMP   label        -> eval env conf (env#labeled label)
+      | CJMP  (cond, label) -> (
+        let (stack, state) = conf in
+        let z :: stack_rest = stack in
+          eval env (stack_rest, state) (
+            if (cond == "z" && z <> 0 || cond == "znz" && z == 0) then rest else (env#labeled label)))
+      | _ -> eval env (execute_instr conf instr) rest
+    )
 
 (* Top-level evaluation
 
@@ -53,4 +83,46 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile p = failwith "Not yet implemented"
+class unique_labels =
+  object
+    val label_n = 0
+    method get_label = {< label_n = label_n + 1 >}, "L" ^ string_of_int label_n
+  end
+
+let rec compile_expr e = match e with
+    | Expr.Var   x -> [LD x]
+    | Expr.Const x -> [CONST x]
+    | Expr.Binop (op, e1, e2) -> compile_expr e1 @ compile_expr e2 @ [BINOP op]
+
+let rec compile_constr id p after_label = match p with
+    | Stmt.Read  x        -> ([READ; ST x]), false
+    | Stmt.Write e        -> (compile_expr e @ [WRITE]),   false
+    | Stmt.Assign (x, e)  -> (compile_expr e @ [ST x]), false
+    | Stmt.Seq    (a, b)  ->  let (id, label) = id#get_label in
+                              let (prg_a, used_a) = compile_constr id a label in
+                              let (prg_b, used_b) = compile_constr id b after_label in
+                              prg_a @ (if used_a then [LABEL label] else []) @ prg_b, used_b
+    | Stmt.Skip -> [], false
+    | Stmt.If (what, first, second) ->
+        let (id, else_label) = id#get_label in
+        let (first_body,  used1) = compile_constr id first  after_label in
+        let (second_body, used2) = compile_constr id second after_label in
+        (compile_expr what) @ [CJMP ("z", else_label)] @
+        first_body  @ (if used1 then [] else [JMP after_label]) @ [LABEL else_label] @
+        second_body @ (if used2 then [] else [JMP after_label]), true
+    | Stmt.While (what, body) -> 
+        let (id, before_label)    = id#get_label in
+        let (id, condition_label) = id#get_label in
+        let (main_body, _) = compile_constr id body condition_label in
+        let condition = compile_expr what in
+        [JMP condition_label; LABEL before_label] @
+        main_body @ [LABEL condition_label] @ condition @ [CJMP ("nz", before_label)], false
+    | Stmt.RepeatUntil (body, what) -> 
+        let (p::rest, _) = (
+          let while_cond = Expr.Binop ("==", what, Expr.Const 0) in
+          compile_constr id (Language.Stmt.While (while_cond, body)) after_label
+          ) in rest, false
+
+let rec compile p = let id, label = (new unique_labels)#get_label in
+                    let prg, used = compile_constr id p label in
+                    prg @ (if used then [LABEL label] else [])
